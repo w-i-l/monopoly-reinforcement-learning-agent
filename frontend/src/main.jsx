28cd0f7c8f53a889ccf1fe5cf817de53a8c1dbaf
Mihai Ocnaru <mihai.ocnaru@mready.net>
2025-07-01 15:31:45 +0300

    added favicon and title

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,20 @@
+import { StrictMode, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./style.css";
+import HumanPlayerInterface from "./components/HumanPlayer/HumanPlayerInterface";
 
-    import { StrictMode } from 'react'
-    import { createRoot } from 'react-dom/client'
-    import './index.css'
-    import './style.css'
-    import HumanPlayerInterface from './components/HumanPlayer/HumanPlayerInterface'
+const root = createRoot(document.getElementById("root"));
 
-    const root = createRoot(document.getElementById('root'))
-    root.render(
-        <StrictMode>
-            <HumanPlayerInterface playerPort={6060} />
-        </StrictMode>
-    )
-    
+document.title = "Monopoly DQN Agent";
+// set image favicon
+const link = document.createElement("link");
+link.rel = "icon";
+link.href = "/favicon.png";
+document.head.appendChild(link);
+
+root.render(
+  <StrictMode>
+    <HumanPlayerInterface playerPort={6060} />
+  </StrictMode>
+);
